5707dba74c83f1e186f57f058d2896c3160579d8
Liubomyr <ljubik@ukr.net>
2020-12-09 17:11:58 +0200

    make template handlebars

---
+++ src/template/template.js
@@ -0,0 +1,9 @@
+import foodListTemplate from '../template/food-list.hbs';
+import menu from '../menu.json';
+
+const markup = foodListTemplate(menu);
+console.log("rozmitka", markup);
+
+const foodListRef = document.querySelector('.js-menu');
+
+foodListRef.insertAdjacentHTML('afterbegin', markup);
